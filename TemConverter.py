c7866059bdc2cd633c7e02dac29bcf0d80f86997
DooorisXu <lingyi19329@gmail.com>
2017-12-23 16:32:31 -0500

    edited the temp converter input

---
+++ TemConverter.py
@@ -0,0 +1,10 @@
+'''coding=utf-8
+this is a tool to convert the temperature from celsius to farenheit and vice versa
+'''
+print("Are you putting in Celsius or Farenheit? ")
+type = input ("")
+temp = int(input("Enter the temperature:"))
+
+
+
+
