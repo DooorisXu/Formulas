d3fcf050bef823ebc176499304e39ae001dedd8e
DooorisXu <lingyi19329@gmail.com>
2017-12-23 19:41:06 -0500

    modified the temp converter--round to two decimal places

--- Temp_converter.py
+++ Temp_converter.py
@@ -5,10 +5,10 @@ temp = int(input(""))
 
 print(unit)
 if unit == "celsius":
-    new = temp*9.0/5.0+32
+    new = "%.2f" %(temp*9.0/5.0+32)
     print('The temperature in Farenheit is: ',new)
 elif unit == "farenheit":
-    new = (temp-32)*5.0/9.0
+    new = "%.2f" %((temp-32)*5.0/9.0)
     print('The temperature in Celsius is: ', new)
 else:
     print('Invalid input.')
